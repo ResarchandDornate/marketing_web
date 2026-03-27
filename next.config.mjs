a738030556fd7c7eabcffea8d21bbb2740790c3e
R&D Team <teams@ornate.in>
2026-03-27 16:08:14 +0530

    new updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['192.168.0.120'],
+  allowedDevOrigins: ['192.168.0.120', '10.189.2.123'],
 };
 
 export default nextConfig;
